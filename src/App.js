b9be3b76fbc46c20d6cc58b642272001251d6cb6
Dheeraj Reddy <f20201134@hyderabad.bits-pilani.ac.in>
2023-10-16 08:19:39 +0530

    Temporary commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [node,setMode] = useState(false);
   return (
     <>
-    <Navbar title="TextUtils"aboutText="About Us"/>
+    {/* <Navbar title="TextUtils"aboutText="About Us"/> */}
     <div className="container my-3">
       <TextForm heading="Enter the text to analyze below"/>
       {/* <About/> */}
